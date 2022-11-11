b8e6d2ab8743231dcf1517c99a062281adb0e594
Edgarcarrog <edgc21@hotmail.com>
2022-11-10 20:02:35 -0600

    appDir config

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head>
+        <title>Test Frontend</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
